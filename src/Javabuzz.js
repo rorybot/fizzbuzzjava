67e6a0c6a833cb28e3ae9063fd068bc7e83f303d
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-09-25 17:19:33 +0100

    first commit

---
+++ src/Javabuzz.js
@@ -0,0 +1,49 @@
+// function Player() {
+// }
+// Player.prototype.play = function(song) {
+//   this.currentlyPlayingSong = song;
+//   this.isPlaying = true;
+// };
+//
+// Player.prototype.pause = function() {
+//   this.isPlaying = false;
+// };
+//
+// Player.prototype.resume = function() {
+//   if (this.isPlaying) {
+//     throw new Error("song is already playing");
+//   }
+//
+//   this.isPlaying = true;
+// };
+//
+// Player.prototype.makeFavorite = function() {
+//   this.currentlyPlayingSong.persistFavoriteStatus(true);
+// };
+
+
+function Fizzbuzz(){
+
+  
+}
+
+
+//
+//
+//
+// def fizzbuzz(number)
+//
+// divis_by_3 = number%3 == 0
+// divis_by_5 = number%5 == 0
+//
+//
+//   if divis_by_3 && divis_by_5
+//     'fizzbuzz'
+//   elsif divis_by_3
+//     'fizz'
+//   elsif divis_by_5
+//     'buzz'
+//   else number
+//   end
+//
+// end
